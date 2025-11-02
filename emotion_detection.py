22c144f3050e7dcf4a901ed65c202d10629ee08b
Warriopradeep <warriopradeep@gmail.com>
2025-11-02 10:27:08 -0500

    Initial Commit

---
+++ emotion_detection.py
@@ -0,0 +1,13 @@
+import requests, json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = {"raw_document": {"text": text_to_analyze}}
+
+    response_from_api = requests.post(url, json = input_json, headers = headers)
+
+    if response_from_api.status_code == 500:
+        return {'label': None, 'score': None}
+
+    return response_from_api.text
