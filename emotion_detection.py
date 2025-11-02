2effcc08f7c0dae5ccefd188f02c4c9f94d038a3
Pradeep <warriopradeep@users.noreply.github.com>
2025-11-02 21:55:23 +0530

    added helper function

--- emotion_detection.py
+++ emotion_detection.py
@@ -12,4 +12,16 @@ def emotion_detector(text_to_analyze):
 
     formatted_response = json.loads(response_from_api.text)
     formatted_response = formatted_response['emotionPredictions'][0]['emotion']
-    return formatted_response
+    return dominant_emotion(formatted_response)
+
+def dominant_emotion(emo_dict):
+    dominant_emo = ""
+    max_val = 0
+
+    for key, value in emo_dict:
+        if value > max_val:
+            max_val = value
+            dominant_emo = key
+
+    emo_dict['dominant_emotion'] = dominant_emo
+    return emo_dict
