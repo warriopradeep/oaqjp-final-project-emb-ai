ea82439dcb4e793fe6af6f8ca9e15b320d67150e
Pradeep <warriopradeep@gmail.com>
2025-11-02 22:13:48 -0500

    Added Error detection

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,8 +8,16 @@ def emotion_detector(text_to_analyze):
 
     response_from_api = requests.post(url, json=input_json, headers=headers)
 
-    if response_from_api.status_code == 500:
-        return {'label': None, 'score': None}
+    # Check if status code is 400 and return dict with all None values
+    if response_from_api.status_code == 400:
+        emo_dict = {'anger': None, 
+                    'disgust': None, 
+                    'fear': None, 
+                    'joy': None,
+                    'sadness': None, 
+                    'dominant_emotion': None}
+
+        return emo_dict
 
     formatted_response = json.loads(response_from_api.text)
     formatted_response = formatted_response['emotionPredictions'][0]['emotion']
--- server.py
+++ server.py
@@ -6,15 +6,18 @@ app = Flask("Emotion Detection")
 @app.route("/emotionDetector")
 def emo_analyzer():
 
-    print(request.args.get('textToAnalyze'))
     text_to_analyze = request.args.get('textToAnalyze')
 
     analysis_output = emotion_detector(text_to_analyze)
 
+    # Check if the input text is blank/innvalid
+    if analysis_output['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
     return (f"For the given statement, the system response is 'anger': {analysis_output['anger']},"
             f" 'disgust': {analysis_output['disgust']}, 'fear': {analysis_output['fear']}, "
             f"'joy': {analysis_output['joy']} and 'sadness': {analysis_output['sadness']}."
-            f" The dominant emotion is {analysis_output['dominant_emotion']}.")
+            f" The dominant emotion is <b>{analysis_output['dominant_emotion']}</b>.")
 
 @app.route("/")
 def render_index_page():
